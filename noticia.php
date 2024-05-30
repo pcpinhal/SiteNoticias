481d6720184334da7dfccd32cd6b41aaa89bc3fb
pcpinhal <pc_pinhal@yahoo.com.br>
2024-05-29 22:23:26 -0300

    por hoje é so !

--- noticia.php
+++ noticia.php
@@ -29,19 +29,21 @@ include_once "conexao.php";
         </nav>
         <main>
             <?php 
-                $sql = "SELECT * FROM tb_noticia ORDER BY RAND() LIMIT 6;";
-                $resultado = mysqli_execute_query($conexao, $sql);
-                while($dados = mysqli_fetch_array($resultado))
+                $id_noticia = @$_GET['id_noticia'];
+                if($id_noticia)
                 {
-                    echo '
-                    <article>
-                        <img src="img/'.$dados["imagem"].'" alt="foto noticia" class="fotonoticia">
-                        <p class="textonoticia">'.$dados["titulo"].'</p>
-                        <a href="noticia.php?id_noticia='.$dados["id_noticia"].'">
-                            <img src="img/saibamais.png" alt="Saiba Mais" class="saibamais">
-                        </a>
-                    </article>
-                    ';                    
+                    echo 'tem conteudo';
+                    $sql = "SELECT * FROM tb_noticia WHERE id_noticia = $id_noticia;";
+                    $resultado = mysqli_execute_query($conexao, $sql);
+                    $dados = mysqli_fetch_array($resultado);
+                    echo "titulo: ".$dados['titulo'].'<br>';
+                    echo "data e hora: ".$dados['datahora'].'<br>';
+                    echo "imagem: ".$dados['imagem'].'<br>';
+                    echo "fonte: ".$dados['fonte'].'<br>';
+                    echo "autor: ".$dados['autor'].'<br>';
+                    echo "noticia: ".$dados['noticia'].'<br>';
+                }else{
+                    echo 'nao tem conteudo';
                 }
             ?>        
         </main>
